4577cbe8f3885ffbddf51b4ec5b286a8833c98ce
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2020-04-22 17:26:28 +0200

    Implement VGG-19 with feature output

--- vgg_19.py
+++ vgg_19.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+
+import torch
 import torch.nn as nn
 import torchvision
 
@@ -8,11 +11,34 @@ class VGG19(nn.Module):
     the first five conv2 layers
     '''
 
-    def __init__(self) -> None:
+    def __init__(self, indexes_of_layers_to_return_features: Tuple[int] = (2, 7, 12, 21, 30)) -> None:
         '''
         Constructor method
+        :param indexes_of_layers_to_return_features: (Tuple[int]) Layers to return feature output
         '''
         # Call super constructor
         super(VGG19, self).__init__()
-        # Load pre-trained vgg-19 from torchvision
-        self.vgg_19 = torchvision.models.vgg19()
+        # Load pre-trained feature part of vgg-19 from torchvision
+        self.vgg_19_features = torchvision.models.vgg19(pretrained=True).features
+        # Convert feature module into model list of layers
+        self.vgg_19_features = nn.ModuleList(list(self.vgg_19_features))
+        # Save parameter
+        self.indexes_of_layers_to_return_features = indexes_of_layers_to_return_features
+
+    def forward(self, input: torch.Tensor) -> List[torch.Tensor]:
+        '''
+        Forward pass
+        :param input: (torch.Tensor) Input image
+        :return: (List[torch.Tenor]) List of features
+        '''
+        # Init list to store features. Could also be implemented with hooks but sometimes problematic with data parallel
+        features = []
+        # Perform forward pass of each layer in vgg-19 feature path
+        for index, layer in enumerate(self.vgg_19_features):
+            # Perform forward pass of layer
+            input = layer(input)
+            # Save output of layer is chosen
+            if index in self.indexes_of_layers_to_return_features:
+                # Feature saved to list
+                features.append(input)
+        return features
