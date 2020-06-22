99c592aa2e070ef502fec7be530392481db86bd9
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2020-06-22 17:08:51 +0200

    Add command line arguments

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
+from argparse import ArgumentParser
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = '0, 2'
+# Manage command line arguments
+parser = ArgumentParser()
+parser.add_argument("--train", default=False, action="store_true",
+                    help="Binary flag. If set training will be performed.")
+parser.add_argument("--val", default=False, action="store_true",
+                    help="Binary flag. If set validation will be performed.")
+parser.add_argument("--test", default=False, action="store_true",
+                    help="Binary flag. If set testing will be performed.")
+parser.add_argument("--inference", default=False, action="store_true",
+                    help="Binary flag. If set testing will be performed.")
+parser.add_argument("--inference_data", default="", type=str,
+                    help="Path to inference data to be loaded.")
+parser.add_argument("--cuda_devices", default="0", type=str,
+                    help="String of cuda device indexes to be used. Indexes must be separated by a comma.")
+parser.add_argument("--data_parallel", default=False, action="store_true",
+                    help="Binary flag. If multi GPU training should be utilized set flag.")
+parser.add_argument("--load_model", default="", type=str,
+                    help="Path to model to be loaded.")
+
+# Get arguments
+args = parser.parse_args()
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda_devices
 
 import torch
 import torch.nn as nn
@@ -14,10 +37,15 @@ from model_wrapper import ModelWrapper
 from dataset import REDS, REDSFovea, REDSParallel, REDSFoveaParallel, reds_parallel_collate_fn
 from lossfunction import AdaptiveRobustLoss
 from resample.resample2d import Resample2d
+import misc
 
 if __name__ == '__main__':
     # Init networks
-    generator_network = nn.DataParallel(RecurrentUNet().cuda())
+    if args.load_model == "":
+        generator_network = nn.DataParallel(RecurrentUNet().cuda())
+    else:
+        generator_network = torch.load(args.load_model)
+    # Init additional models
     discriminator_network = nn.DataParallel(Discriminator().cuda())
     fft_discriminator_network = nn.DataParallel(FFTDiscriminator().cuda())
     vgg_19 = nn.DataParallel(VGG19().cuda())
@@ -50,4 +78,16 @@ if __name__ == '__main__':
                                      num_workers=1),
                                  test_dataloader=None)
     # Perform training
-    model_wrapper.train(epochs=20)
+    if args.train:
+        model_wrapper.train(epochs=20)
+    # Perform final validation
+    if args.val:
+        model_wrapper.validate()
+    # Perform testing
+    if args.test:
+        model_wrapper.test()
+    # Perform validation
+    if args.inference:
+        # Load data
+        inference_data = misc.load_inference_data(args.inference_data)
+        model_wrapper.inference(inference_data)
